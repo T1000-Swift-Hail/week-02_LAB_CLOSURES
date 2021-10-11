b073292fc97fb7cdc70a991b0271784182860001
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-11 13:45:04 +0300

    OK Done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let exportsAmount = [4566, 231, 8739, 2476, 6529, 1343]
+
+func country(i: Int, j: Int) -> Bool {
+    return i > j
+}
+let sortedexportsAmount = exportsAmount.sorted(by: country)
+print(sortedexportsAmount)
