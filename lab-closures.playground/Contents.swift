85c2d0c6bb6ce5b317e68cf002c8165c44b0641b
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-11 14:37:53 +0300

    lap closures

---
+++ lab-closures.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+import Foundation
+
+let exportsAmount=[4566,231,8739,2476,6529,1343]
+let sortedexportsAmount=exportsAmount.sorted(by:{(i:Int,j:Int)->Bool in
+    return i  > j })
+   //print (sortedexportsAmount)
+
+let exports = [4566,231,8739,2476,6529,1343]
+
+let sortedexports=exports.max(by:{
+    (i:Int,j:Int)-> Bool in
+    
+    return i < j
+})
+print(sortedexports ?? 1)
+
