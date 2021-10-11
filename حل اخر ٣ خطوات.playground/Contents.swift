a9755f0c1004123a011d3f9dcd247d4da76dee39
Huda N S <hnsa5@MacBook-Air-Huda.local>
2021-10-11 13:47:30 +0300

    done

---
+++ حل اخر ٣ خطوات.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var exportsAmount : Array <Int> = [4566, 231, 8739, 2476, 6529, 1343]
+
+print("Ranking from large to smaller")
+let exportsAmount2 = exportsAmount.sorted { $0 > $1 }
+print(exportsAmount2)
+
+
+let TopexportsAmount = exportsAmount.max()
+print("max value from the exportsAmount")
+print(TopexportsAmount ?? 1)
